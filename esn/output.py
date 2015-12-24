3afdfc0446c27677e471b805d4c2b738a5c8c034
jadewizzard <dima@buryakov.in>
2015-12-24 17:45:05 +0300

    вывод с аргументом

--- esn/output.py
+++ esn/output.py
@@ -5,10 +5,20 @@
 """
 
 import app
+import sys
 
 check = app.InitialCheck()
 vk = app.VKontakte()
 
-if check.conky():
+app_arg = sys.argv[1]
+# получаем аргумент переданный программе
+# первый аргумент передоваемый программе
+# это указание на сервис из которого нужно
+# получить кол-во непрочитанных сообщений / писем
+# соответственно:
+# -vk - Вконтакте
+# -tm - Telegramm
+
+if app_arg == "-vk":
     if check.vk():
         vk.getUnreadMessage()
